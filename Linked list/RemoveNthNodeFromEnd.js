50553d4bf0b7d696797559801daafce4c38da2b6
SumeetHaryani <sumeetharyani3@gmail.com>
2022-04-18 19:04:02 +0530

    add coments

--- Linked list/RemoveNthNodeFromEnd.js
+++ Linked list/RemoveNthNodeFromEnd.js
@@ -1,11 +1,14 @@
 var removeNthFromEnd = function (head, n) {
   let slow = (fast = head),
     index = 0;
+  //Move fast in front so that the gap between slow and fast becomes n
+
   while (index < n) {
     fast = fast.next;
     index++;
   }
   if (!fast) return head.next;
+  //Move fast to the end, maintaining the gap
 
   while (fast.next) {
     slow = slow.next;
